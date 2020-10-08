11f258c245c13d44b7e417697aebdc0dd51130a0
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2020-10-08 20:52:06 +0530

    lab4 done

--- getcount.c
+++ getcount.c
@@ -5,16 +5,17 @@
 int
 main(int argc, char *argv[])
 {
-    printf(1, "initial fork count %d\n", getcount());
-    if (fork() == 0) {
-        printf(1, "child fork count %d\n", getcount());
-        printf(1, "child write count %d\n", getcount());
-    } else {
-        wait();
-        printf(1, "parent fork count %d\n", getcount());
-        printf(1, "parent write count %d\n", getcount());
-    }
-    printf(1, "wait count %d\n", getcount());
+    printf(1, "check = %d\n", getcount());
+//    printf(1, "initial fork count %d\n", getcount());
+//    if (fork() == 0) {
+//        printf(1, "child fork count %d\n", getcount());
+//        printf(1, "child write count %d\n", getcount());
+//    } else {
+//        wait();
+//        printf(1, "parent fork count %d\n", getcount());
+//        printf(1, "parent write count %d\n", getcount());
+//    }
+//    printf(1, "wait count %d\n", getcount());
     exit();
 }
 
