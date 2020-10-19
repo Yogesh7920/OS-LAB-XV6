cd9e3ef2df79411d83d614b98e9c25f1adeb6f10
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2020-10-19 20:47:29 +0530

    Lab5 completed

--- test_v2paddr.c
+++ test_v2paddr.c
@@ -9,13 +9,13 @@ main(int argc, char *argv[])
 
   uint pa;
   int arr[2] = {0};
+  uint *x1;
 
   if (!v2paddr(&pa, &arr[0]))
     printf(1, "Physical Address = %x\n", pa);
 
   if (!v2paddr(&pa, &arr[1]))
     printf(1, "Physical Address = %x\n", pa);
-  uint *x1;
   x1 = (uint *)0x2321;
   if (!v2paddr(&pa, x1))
     printf(1, "Physical Address = %x\n", pa);
@@ -25,7 +25,6 @@ main(int argc, char *argv[])
     printf(1, "Physical Address = %x\n", pa);
 
   x1 = (uint *)0x3001;
-
   if (!v2paddr(&pa, x1))
     printf(1, "Physical Address = %x\n", pa);
 
